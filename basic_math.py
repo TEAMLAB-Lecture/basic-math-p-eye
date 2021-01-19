104056654eba8c068fda385d8741f41891a4f2a0
Eun young Park <eun-park@student.42.fr>
2021-01-19 17:46:07 +0900

    Update my code

--- basic_math.py
+++ basic_math.py
@@ -24,7 +24,10 @@ def get_greatest(number_list):
             >>> bm.get_greatest(number_list)
             99
     """
-    greatest_number = None
+    greatest_number = number_list[0]
+    for i in number_list:
+        if (greatest_number < i):
+            greatest_number = i
     return greatest_number
 
 
@@ -45,7 +48,11 @@ def get_smallest(number_list):
             >>> bm.get_smallest(number_list)
             11
     """
-    smallest_number = None
+    smallest_number = number_list[0]
+    i = 0
+    for i in number_list:
+        if (smallest_number > i):
+            smallest_number = i
     return smallest_number
 
 
@@ -66,7 +73,10 @@ def get_mean(number_list):
             >>> bm.get_mean(number_list)
             47
     """
-    mean = None
+    sum = 0
+    for i in number_list:
+        sum += i
+    mean = sum / len(number_list)
     return mean
 
 
@@ -90,5 +100,9 @@ def get_median(number_list):
             >>> bm.get_median(number_list2)
             35.5
     """
-    median = None
-    return median
+    number_list.sort()
+    size = len(number_list)
+    if size % 2 == 1:
+        return number_list[size // 2]
+    else:
+        return (number_list[size // 2] + number_list[size // 2 - 1]) / 2
